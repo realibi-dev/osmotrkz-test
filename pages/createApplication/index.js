e74b4713f04ce59cbae3a5d8bb90b07a908c310c
alibi duisenaliyev <realibi.dev@gmail.com>
2023-12-19 21:43:37 +0600

    a

--- pages/createApplication/index.js
+++ pages/createApplication/index.js
@@ -117,42 +117,46 @@ export default function CreateApplication() {
     }
 
     function openPaymentWidgetHandler() {
-        openPaymentWidget({
-            api_key: '8590a7d1-cfb1-41bf-9619-1c333a14f960',
-            amount: 1500,
-            currency: "KZT",
-            order_id: Math.round(Math.random() * 100000).toString(),
-            description: "description",
-            payment_type: "pay",
-            payment_method: "ecom",
-            items: [{
-                merchant_id: "f523e618-baf9-46a5-b841-3a7d3451aa46",
-                service_id: "b6549f27-2a36-4166-bc94-46f29e026f81",
-                merchant_name: "Merchant name",
-                name: "Example",
-                quantity: 1,
-                amount_one_pcs: 1500,
-                amount_sum: 1500,
-            }],
-            user_id: getCurrentUser().id,
-            email: getCurrentUser().email,
-            phone: getCurrentUser().phone,
-            success_url: "https://osmotrkz.vercel.app/createApplication/success",
-            failure_url: "https://osmotrkz.vercel.app/createApplication/success",
-            callback_url: "https://osmotrkz.vercel.app/createApplication/success",
-            payment_lifetime: 600,
-            create_recurrent_profile: false,
-            recurrent_profile_lifetime: 0,
-            lang: "ru",
-            extra_params: {},
-            payment_gateway_host: "https://api.onevisionpay.com",
-            payment_widget_host: "https://widget.onevisionpay.com",
-            test_mode: 1,
-        }, 
-        (success) => {
-            createApplicationHandler();
-        },
-        (error) => { alert(error) });
+        if (getCurrentUser().id) {
+            openPaymentWidget({
+                api_key: '8590a7d1-cfb1-41bf-9619-1c333a14f960',
+                amount: 10,
+                currency: "KZT",
+                order_id: Math.round(Math.random() * 100000).toString(),
+                description: "description",
+                payment_type: "pay",
+                payment_method: "ecom",
+                items: [{
+                    merchant_id: "f523e618-baf9-46a5-b841-3a7d3451aa46",
+                    service_id: "b6549f27-2a36-4166-bc94-46f29e026f81",
+                    merchant_name: "Merchant name",
+                    name: "Example",
+                    quantity: 1,
+                    amount_one_pcs: 1500,
+                    amount_sum: 1500,
+                }],
+                user_id: getCurrentUser().id.toString(),
+                email: getCurrentUser().email,
+                phone: getCurrentUser().phone,
+                success_url: "https://osmotrkz.vercel.app/createApplication/success",
+                failure_url: "https://osmotrkz.vercel.app/createApplication/success",
+                callback_url: "https://osmotrkz.vercel.app/createApplication/success",
+                payment_lifetime: 600,
+                create_recurrent_profile: false,
+                recurrent_profile_lifetime: 0,
+                lang: "ru",
+                extra_params: {},
+                payment_gateway_host: "https://api.onevisionpay.com",
+                payment_widget_host: "https://widget.onevisionpay.com",
+                test_mode: 1,
+            }, 
+            (success) => {
+                createApplicationHandler();
+            },
+            (error) => { alert(error) });
+        } else {
+            router.push('/login');
+        }
     }
 
     return (
